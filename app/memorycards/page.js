57d950f4950e90f3a3fefaaa24b5bc86875fee28
Mark Christian <chcknknight@gmail.com>
2024-10-27 01:12:07 -0400

    little fix

--- app/memorycards/page.js
+++ app/memorycards/page.js
@@ -1,6 +1,6 @@
 'use client'
 import React, {useState} from 'react';
-import FlashcardList from './flashcardlist';
+import FlashcardList from './FlashcardList';
 import './app.css'
 
 function App(){
